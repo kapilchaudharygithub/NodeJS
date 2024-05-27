e3c56074ba690c0f15702b95e7894a57f2ed93ef
Kapil Chaudhary <kcgithub2@gmail.com>
2024-05-27 07:12:33 +0530

    Hello World

---
+++ Level_I/I_HelloWorld.js
@@ -0,0 +1,12 @@
+/** Learning how to Print Hello World in NodeJS */
+console.log("Hello World")
+/**
+ * Run:
+ * node fileName.js
+ *    or
+ * node filename
+ */
+
+/**
+ * DOM and window related things are not there in NodeJS which are present in Browser , because it is used for server side not on client side(UI)
+ */
